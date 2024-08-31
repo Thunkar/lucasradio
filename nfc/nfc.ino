374f956f1919a1209a1937a6cb906abae67787f3
thunkar <gregojquiros@gmail.com>
2024-08-31 19:34:49 +0200

    integration wip

--- nfc/nfc.ino
+++ nfc/nfc.ino
@@ -3,6 +3,8 @@
 #include <PN532.h>
 #include <NfcAdapter.h>
 
+#define NDEF_USE_SERIAL 0;
+
 PN532_I2C pn532i2c(Wire);
 NfcAdapter nfc = NfcAdapter(pn532i2c);
 
@@ -11,94 +13,35 @@ void setup(void) {
   while(!Serial) {
     ;
   }
-  Serial.println("Hello!");
 
   nfc.begin();
-
-  // //uint32_t versiondata = nfc.getFirmwareVersion();
-  // if (! versiondata) {
-  //   Serial.print("Didn't find PN53x board");
-  //   while (1); // halt
-  // }
-  
-  // // Got ok data, print it out!
-  // Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX); 
-  // Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC); 
-  // Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
-  
-  // // Set the max number of retry attempts to read from a card
-  // // This prevents us from waiting forever for a card, which is
-  // // the default behaviour of the PN532.
-  // nfc.setPassiveActivationRetries(0xFF);
-  
-  // // configure board to read RFID tags
-  // nfc.SAMConfig();
-    
-  Serial.println("Waiting for an ISO14443A card");
 }
 
 void loop(void) {
-  boolean success;
-  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
-  uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
-  
-  // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
-  // 'uid' will be populated with the UID, and uidLength will indicate
-  // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
-  //success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength);
-  
    if (nfc.tagPresent())
   {
     NfcTag tag = nfc.read();
-    Serial.println(tag.getTagType());
-    Serial.print("UID: ");Serial.println(tag.getUidString());
 
-    if (tag.hasNdefMessage()) // every tag won't have a message
+    if (tag.hasNdefMessage())
     {
 
       NdefMessage message = tag.getNdefMessage();
-      Serial.print("\nThis NFC Tag contains an NDEF Message with ");
-      Serial.print(message.getRecordCount());
-      Serial.print(" NDEF Record");
-      if (message.getRecordCount() != 1) {
-        Serial.print("s");
-      }
-      Serial.println(".");
 
-      // cycle through the records, printing some info from each
       int recordCount = message.getRecordCount();
       for (int i = 0; i < recordCount; i++)
       {
-        Serial.print("\nNDEF Record ");Serial.println(i+1);
         NdefRecord record = message.getRecord(i);
-        // NdefRecord record = message[i]; // alternate syntax
-
-        Serial.print("  TNF: ");Serial.println(record.getTnf());
-        Serial.print("  Type: ");Serial.println(record.getType()); // will be "" for TNF_EMPTY
-
-        // The TNF and Type should be used to determine how your application processes the payload
-        // There's no generic processing for the payload, it's returned as a byte[]
         int payloadLength = record.getPayloadLength();
         byte payload[payloadLength];
         record.getPayload(payload);
 
-        // Print the Hex and Printable Characters
-        Serial.print("  Payload (HEX): ");
-        PrintHexChar(payload, payloadLength);
-
-        // Force the data into a String (might work depending on the content)
-        // Real code should use smarter processing
         String payloadAsString = "";
-        for (int c = 0; c < payloadLength; c++) {
+        for (int c = 3; c < payloadLength; c++) {
           payloadAsString += (char)payload[c];
         }
-        Serial.print("  Payload (as String): ");
-        Serial.println(payloadAsString);
 
-        // id is probably blank and will return ""
-        String uid = record.getId();
-        if (uid != "") {
-          Serial.print("  ID: ");Serial.println(uid);
+        if(payloadAsString.endsWith(".mp3")) {
+          Serial.println(payloadAsString);
         }
       }
     }
